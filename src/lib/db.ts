b3e433f3f6b01b663d03dbefa13c57d5590d5ae0
Nulo <git@nulo.in>
2024-02-10 12:11:09 -0300

    activar WAL

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,13 +1,11 @@
-import {
-  drizzle,
-  type BetterSQLite3Database,
-} from "drizzle-orm/better-sqlite3";
+import { drizzle } from "drizzle-orm/better-sqlite3";
 import { migrate } from "drizzle-orm/better-sqlite3/migrator";
 import Database from "better-sqlite3";
 import * as schema from "../schema.js";
 
 async function connectDb() {
   const sqlite = new Database("sqlite.db");
+  await sqlite.exec("PRAGMA journal_mode=WAL;");
   const db = drizzle(sqlite, { schema });
   await migrate(db, { migrationsFolder: "drizzle" });
   return db;
