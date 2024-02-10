510af2bb6d84e5d4edfad41640b047bf48357458
Nulo <git@nulo.in>
2024-02-10 12:38:24 -0300

    twits nuevos primero

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,7 +1,11 @@
 import { db } from "$lib/db";
+import { desc } from "drizzle-orm";
+import { likedTweets } from "../schema";
 import type { PageServerLoad } from "./$types";
 
 export const load: PageServerLoad = async ({ params }) => {
-  const tweets = await db.query.likedTweets.findMany();
+  const tweets = await db.query.likedTweets.findMany({
+    orderBy: desc(likedTweets.firstSeenAt),
+  });
   return { tweets };
 };
