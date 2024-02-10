fee93af4667f26c995c1bd2dd4c121a25fbb0548
Nulo <git@nulo.in>
2024-02-10 15:30:13 -0300

    elegir db_path

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -2,9 +2,10 @@ import { drizzle } from "drizzle-orm/better-sqlite3";
 import { migrate } from "drizzle-orm/better-sqlite3/migrator";
 import Database from "better-sqlite3";
 import * as schema from "../schema.js";
+import { env } from "$env/dynamic/private";
 
 async function connectDb() {
-  const sqlite = new Database("sqlite.db");
+  const sqlite = new Database(env.DB_PATH ?? "sqlite.db");
   await sqlite.exec("PRAGMA journal_mode=WAL;");
   const db = drizzle(sqlite, { schema });
   await migrate(db, { migrationsFolder: "drizzle" });
