08a132f60d1b3427cf39cf2ed1a94147dba18bf4
Nulo <git@nulo.in>
2024-02-15 20:57:55 -0300

    configurar cache para cloudflare

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -3,12 +3,17 @@ import { desc } from "drizzle-orm";
 import { likedTweets, scraps } from "../schema";
 import type { PageServerLoad } from "./$types";
 
-export const load: PageServerLoad = async ({ params }) => {
+export const load: PageServerLoad = async ({ params, setHeaders }) => {
   const tweets = await db.query.likedTweets.findMany({
     orderBy: desc(likedTweets.firstSeenAt),
   });
   const lastUpdated = await db.query.likedTweets.findFirst({
     orderBy: desc(likedTweets.firstSeenAt),
   });
+
+  setHeaders({
+    "cache-control": "public, max-age=1800",
+  });
+
   return { tweets, lastUpdated };
 };
