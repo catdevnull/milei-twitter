121f49d89270d29b8e257f47ed0e2efad413811d
Nulo <git@nulo.in>
2024-02-16 00:35:01 -0300

    mejorar contraste

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -255,14 +255,14 @@
     <div>
       Compartir por
       <a
-        class="rounded bg-green-500 px-3 py-2 text-white"
+        class="rounded bg-green-600 px-3 py-2 font-medium text-white"
         href={`https://api.whatsapp.com/send?text=${encodeURIComponent("¿Cuántos tweets likeó nuestro Presidente las últimas 24 horas? https://milei.nulo.in/?ref=wsp-link")}`}
         >WhatsApp</a
       >
     </div>
     <div>
       hecho por <a
-        class="text-blue-600 underline dark:text-blue-300"
+        class="text-blue-600 underline dark:text-blue-200"
         href="https://twitter.com/esoesnulo"
         rel="noreferrer">@esoesnulo</a
       >
