e2944cd05ca4eba178e767e9decc21460a7fb398
Nulo <git@nulo.in>
2024-02-18 21:09:47 -0300

    refactor, reimplement cron

--- scraper/index.ts
+++ scraper/index.ts
@@ -19,7 +19,8 @@ const scrapLikesCommand = command({
   async handler({ n }) {
     const db = await connectDb(process.env.DB_PATH);
     const scraper = new Scraper(db);
-    await scraper.scrap(n);
+    const cuenta = await this.getRandomAccount();
+    await scraper.scrap(cuenta, n);
     await scraper.browser?.close();
   },
 });
@@ -37,7 +38,8 @@ const scrapRetweetsCommand = command({
   async handler({ save, n, saveApiResponses }) {
     const db = await connectDb(process.env.DB_PATH);
     const scraper = new Scraper(db);
-    const result = await scraper.scrapTweets({ n, saveApiResponses });
+    const cuenta = await scraper.getRandomAccount();
+    const result = await scraper.scrapTweets({ n, saveApiResponses, cuenta });
     console.log(result);
     if (save) await scraper.saveTweetsScrap(result);
     await scraper.browser?.close();
@@ -47,9 +49,18 @@ const scrapCommand = subcommands({
   name: "scrap",
   cmds: { likes: scrapLikesCommand, retweets: scrapRetweetsCommand },
 });
+const cronCommand = command({
+  name: "cron",
+  args: {},
+  async handler({}) {
+    const db = await connectDb(process.env.DB_PATH);
+    const scraper = new Scraper(db);
+    await scraper.cron();
+  },
+});
 const app = subcommands({
   name: "milei-twitter-scraper",
-  cmds: { scrap: scrapCommand },
+  cmds: { scrap: scrapCommand, cron: cronCommand },
 });
 run(app, process.argv.slice(2));
 
@@ -100,7 +111,7 @@ const zUserTweetsTweet = z.object({
 });
 const zUserTweetsRetweetTweet = z.object({
   created_at: z.coerce.date(),
-  user_id_str: z.literal(JMILEI_ID), // id de @JMilei
+  user_id_str: z.literal(JMILEI_ID),
   id_str: z.string(),
   retweeted_status_result: z.object({
     result: z.object({
@@ -194,13 +205,8 @@ class Scraper {
     this.db = db;
   }
 
-  async scrap(n: number | undefined = undefined) {
-    const cuenta = await this.db.query.cuentas.findFirst({
-      orderBy: sql`random()`,
-    });
+  async scrap(cuenta: schema.Cuenta, n: number | undefined = undefined) {
     try {
-      if (!cuenta) throw new Error("no tengo cuentas para scrapear");
-
       const scrap = await this.db
         .insert(schema.scraps)
         .values({ at: new Date(), cuentaId: cuenta.id })
@@ -225,8 +231,20 @@ class Scraper {
   }
 
   async cron() {
+    let i = 0;
     while (true) {
-      await this.scrap();
+      const cuenta = await this.getRandomAccount();
+      await this.scrap(cuenta);
+      i--;
+      if (i <= 0) {
+        try {
+          const result = await this.scrapTweets({ cuenta });
+          await this.saveTweetsScrap(result);
+        } catch (error) {
+          console.error(`tweets[${cuenta.id}]:`, error);
+        }
+        i = 15;
+      }
       await wait(50 * 1000 + Math.random() * 15 * 1000);
     }
   }
@@ -249,113 +267,115 @@ class Scraper {
   async scrapTweets({
     n = 5,
     saveApiResponses = false,
+    cuenta,
   }: {
     n?: number;
     saveApiResponses?: boolean;
+    cuenta: schema.Cuenta;
   }): Promise<TweetsScrapResult> {
     // TODO: scrapear tuits propios
 
-    // setup browser
-    const cuenta = await this.getRandomAccount();
-    const page = await this.newPage();
-    this.setupAccountInPage(cuenta, page);
+    return await this.usePage(async (page) => {
+      this.setupAccountInPage(cuenta, page);
 
-    let map = new Map<string, UserTweetsTweet | UserTweetsRetweetTweet>();
+      let map = new Map<string, UserTweetsTweet | UserTweetsRetweetTweet>();
 
-    // setup API response capturing and parsing
-    page.on("response", async (response) => {
-      const req = response.request();
-      const url = req.url();
-      if (
-        url.includes("/graphql/") &&
-        url.includes("UserTweets?") &&
-        req.method() === "GET"
-      ) {
-        const json = await response.json();
-        if (saveApiResponses) {
-          await mkdir("debug-api-responses", { recursive: true });
-          await writeFile(
-            `debug-api-responses/${+new Date()}-${nanoid()}.json`,
-            JSON.stringify(json, undefined, 2),
-          );
-        }
-
-        const parsed = zUserTweetsRes.parse(json);
-        const entries =
-          parsed.data.user.result.timeline_v2.timeline.instructions
-            .filter(
-              (x): x is z.infer<typeof zTimelineAddEntriesEntry> =>
-                "entries" in x,
-            )
-            .flatMap((x) =>
-              x.entries
-                .map((e) => e.content)
-                .filter(
-                  (y): y is TimelineTimelineItem =>
-                    y.entryType === "TimelineTimelineItem",
-                ),
-            )
-            // filtrar publicidades
-            .filter((e) => !e.itemContent.tweet_results.promotedMetadata)
-            .map((e) => e.itemContent.tweet_results.result)
-            // filtrar publicidades
-            .filter(
-              (e): e is z.infer<typeof zUserTweetsTweetResultTweet> =>
-                e.__typename === "Tweet",
+      // setup API response capturing and parsing
+      page.on("response", async (response) => {
+        const req = response.request();
+        const url = req.url();
+        if (
+          url.includes("/graphql/") &&
+          url.includes("UserTweets?") &&
+          req.method() === "GET"
+        ) {
+          const json = await response.json();
+          if (saveApiResponses) {
+            await mkdir("debug-api-responses", { recursive: true });
+            await writeFile(
+              `debug-api-responses/${+new Date()}-${nanoid()}.json`,
+              JSON.stringify(json, undefined, 2),
             );
-        for (const entry of entries) {
-          map.set(entry.legacy.id_str, entry.legacy);
+          }
+
+          const parsed = zUserTweetsRes.parse(json);
+          const entries =
+            parsed.data.user.result.timeline_v2.timeline.instructions
+              .filter(
+                (x): x is z.infer<typeof zTimelineAddEntriesEntry> =>
+                  "entries" in x,
+              )
+              .flatMap((x) =>
+                x.entries
+                  .map((e) => e.content)
+                  .filter(
+                    (y): y is TimelineTimelineItem =>
+                      y.entryType === "TimelineTimelineItem",
+                  ),
+              )
+              // filtrar publicidades
+              .filter((e) => !e.itemContent.tweet_results.promotedMetadata)
+              .map((e) => e.itemContent.tweet_results.result)
+              // filtrar publicidades
+              .filter(
+                (e): e is z.infer<typeof zUserTweetsTweetResultTweet> =>
+                  e.__typename === "Tweet",
+              );
+          for (const entry of entries) {
+            map.set(entry.legacy.id_str, entry.legacy);
+          }
         }
+      });
+
+      // go to page and scroll down to load more
+      await page.goto("https://twitter.com/jmilei");
+      for (let i = 0; i < n; i++) {
+        const sel = `a[dir="ltr"] > time`;
+
+        // scrollear al final para permitir que mas se cargen
+        await page.waitForSelector(sel);
+        const els = await page.$$(sel);
+        await els[els.length - 1].scrollIntoView();
+
+        // esperar para cargar tweets
+        await wait(1000);
       }
-    });
 
-    // go to page and scroll down to load more
-    await page.goto("https://twitter.com/jmilei");
-    for (let i = 0; i < n; i++) {
-      const sel = `a[dir="ltr"] > time`;
-
-      // scrollear al final para permitir que mas se cargen
-      await page.waitForSelector(sel);
-      const els = await page.$$(sel);
-      await els[els.length - 1].scrollIntoView();
-
-      // esperar para cargar tweets
-      await wait(1000);
-    }
-
-    // filter tweets, separate into retweets and convert into our DB format
-    const allTweets = Array.from(map.values()).filter((tweet) => {
-      if (tweet.user_id_str !== JMILEI_ID) {
-        console.warn(
-          `tweet que no es de milei en feed https://twitter.com/${tweet.user_id_str}/status/${tweet.id_str}`,
+      // filter tweets, separate into retweets and convert into our DB format
+      const allTweets = Array.from(map.values()).filter((tweet) => {
+        if (tweet.user_id_str !== JMILEI_ID) {
+          // esto suelen ser publicidades que no me fije bien como filtrar
+          console.warn(
+            `tweet que no es de milei en feed https://twitter.com/${tweet.user_id_str}/status/${tweet.id_str}`,
+          );
+          return false;
+        }
+        return true;
+      });
+      const retweets = allTweets
+        .filter(
+          (tweet): tweet is UserTweetsRetweetTweet =>
+            "retweeted_status_result" in tweet,
+        )
+        .map(
+          (tweet): schema.Retweet => ({
+            url: `https://twitter.com/${tweet.user_id_str}/status/${tweet.id_str}`,
+            firstSeenAt: new Date(),
+            postedAt: tweet.retweeted_status_result.result.legacy.created_at,
+            retweetAt: tweet.created_at,
+            text: tweet.retweeted_status_result.result.legacy.full_text,
+          }),
         );
-        return false;
-      }
-      return true;
+
+      // XXX: ojo que puede ser que estemos contando tweets aunque no estemos logeadxs.. pero son tweets viejos populares que twitter muestra cuando no estamos logeadxs
+      const totalTweetsSeen = map.size;
+
+      return {
+        tweetsSeen: totalTweetsSeen,
+        retweets,
+        cuenta,
+      };
     });
-    const retweets = allTweets
-      .filter(
-        (tweet): tweet is UserTweetsRetweetTweet =>
-          "retweeted_status_result" in tweet,
-      )
-      .map(
-        (tweet): schema.Retweet => ({
-          url: `https://twitter.com/${tweet.user_id_str}/status/${tweet.id_str}`,
-          firstSeenAt: new Date(),
-          postedAt: tweet.retweeted_status_result.result.legacy.created_at,
-          retweetAt: tweet.created_at,
-          text: tweet.retweeted_status_result.result.legacy.full_text,
-        }),
-      );
-
-    // XXX: ojo que puede ser que estemos contando tweets aunque no estemos logeadxs.. pero son tweets viejos populares que twitter muestra cuando no estamos logeadxs
-    const totalTweetsSeen = map.size;
-
-    return {
-      tweetsSeen: totalTweetsSeen,
-      retweets,
-      cuenta,
-    };
   }
 
   async saveTweetsScrap({ retweets, tweetsSeen, cuenta }: TweetsScrapResult) {
@@ -369,9 +389,12 @@ class Scraper {
       .returning();
     const scrapId = scrap[0].id;
     // TODO: guardar tweets propios
-    await this.db
-      .insert(schema.retweets)
-      .values(retweets.map((r) => ({ ...r, scrapId })));
+    for (const retweet of retweets.map((r) => ({ ...r, scrapId }))) {
+      await this.db
+        .insert(schema.retweets)
+        .values(retweet)
+        .onConflictDoNothing();
+    }
   }
 
   /**
@@ -403,8 +426,6 @@ class Scraper {
       sel,
     );
 
-    console.debug(got);
-
     for (const { href, text } of got) {
       let q = this.db
         .insert(schema.likedTweets)
@@ -430,53 +451,63 @@ class Scraper {
     cuenta: schema.Cuenta,
     scrapId: number,
   ): Promise<number> {
-    const page = await this.newPage();
-    page.setCookie(...cookiesFromAccountData(cuenta));
-
-    {
-      await page.goto("https://twitter.com/");
+    return await this.usePage(async (page) => {
+      await this.setupAccountInPage(cuenta, page);
       {
-        const searchBoxEl = `[data-testid="SearchBox_Search_Input"]`;
-        await page.waitForSelector(searchBoxEl);
-        await page.type(searchBoxEl, "javier milei");
+        await page.goto("https://twitter.com/");
+        await page.reload();
+        {
+          const searchBoxEl = `[data-testid="SearchBox_Search_Input"]`;
+          await page.waitForSelector(searchBoxEl);
+          await page.type(searchBoxEl, "javier milei");
+        }
+        {
+          const jmileiSelector = `[data-testid="sidebarColumn"] >>> ::-p-text(@JMilei)`;
+          await page.waitForSelector(jmileiSelector);
+          await wait(1500);
+          await page.click(jmileiSelector);
+        }
+        {
+          const likesLinkSelector = `a[href="/JMilei/likes"]`;
+          await page.waitForSelector(likesLinkSelector);
+          await page.click(likesLinkSelector);
+        }
       }
-      {
-        const jmileiSelector = `[data-testid="sidebarColumn"] >>> ::-p-text(@JMilei)`;
-        await page.waitForSelector(jmileiSelector);
+
+      let count = 0;
+
+      for (let i = 0; i < n; i++) {
+        const sel = `[aria-label="Timeline: Javier Milei’s liked posts"] a[dir="ltr"] > time`;
+
+        // scrapear tweets y guardarlos
+        count += await this.saveLikedTweets(page, scrapId);
+
+        // scrollear al final para permitir que mas se cargenrfdo
+        const els = await page.$$(sel);
+        await els[els.length - 1].scrollIntoView();
+
+        // esperar para cargar tweets
         await wait(1500);
-        await page.click(jmileiSelector);
       }
-      {
-        const likesLinkSelector = `a[href="/JMilei/likes"]`;
-        await page.waitForSelector(likesLinkSelector);
-        await page.click(likesLinkSelector);
-      }
-    }
 
-    let count = 0;
-
-    for (let i = 0; i < n; i++) {
-      const sel = `[aria-label="Timeline: Javier Milei’s liked posts"] a[dir="ltr"] > time`;
-
-      // scrapear tweets y guardarlos
-      count += await this.saveLikedTweets(page, scrapId);
-
-      // scrollear al final para permitir que mas se cargenrfdo
-      const els = await page.$$(sel);
-      await els[els.length - 1].scrollIntoView();
-
-      // esperar para cargar tweets
-      await wait(1500);
-    }
-
-    return count;
+      return count;
+    });
   }
 
-  async newPage() {
-    const browser = await this.getBrowser();
-    const page = await browser.newPage();
-    await page.setViewport({ width: 1080, height: 1024 });
-    return page;
+  async usePage<T>(callback: (page: Page) => Promise<T>): Promise<T> {
+    const newPage = async () => {
+      const browser = await this.getBrowser();
+      const page = await browser.newPage();
+      await page.setViewport({ width: 1080, height: 1024 });
+      return page;
+    };
+
+    const page = await newPage();
+    try {
+      return await callback(page);
+    } finally {
+      page.close();
+    }
   }
 
   async getBrowser() {
@@ -487,7 +518,7 @@ class Scraper {
         });
       } else {
         this.browser = await puppeteer.launch({
-          // headless: false,
+          headless: false,
         });
       }
     }
@@ -498,5 +529,3 @@ class Scraper {
 function wait(ms: number) {
   return new Promise((resolve) => setTimeout(resolve, ms));
 }
-
-(async () => {})();
