f79b143290c4001b7f73759ff178fe1c9001289d
Nulo <git@nulo.in>
2024-02-19 13:52:39 -0300

    partial revert drizzle

--- drizzle/0006_shocking_trauma.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE `db_retweets` (
-	`url` text PRIMARY KEY NOT NULL,
-	`first_seen_at` integer NOT NULL,
-	`retweet_at` integer NOT NULL,
-	`posted_at` integer NOT NULL,
-	`text` text,
-	`scrap_id` integer
-);
