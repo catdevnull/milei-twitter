bb4227b697817bbb7c3a234977f3bdc10f77b8d8
Nulo <git@nulo.in>
2024-02-19 16:25:36 -0300

    conseguir ultimo scrap for lastupdated
    fixes #28

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,5 +1,5 @@
 import { db } from "$lib/db";
-import { desc } from "drizzle-orm";
+import { desc, isNotNull } from "drizzle-orm";
 import { likedTweets, retweets, scraps } from "../schema";
 import type { PageServerLoad } from "./$types";
 
@@ -20,8 +20,9 @@ export const load: PageServerLoad = async ({ params, setHeaders }) => {
     },
     orderBy: desc(retweets.retweetAt),
   });
-  const lastUpdated = await db.query.likedTweets.findFirst({
-    orderBy: desc(likedTweets.firstSeenAt),
+  const lastUpdated = await db.query.scraps.findFirst({
+    orderBy: desc(scraps.at),
+    where: isNotNull(scraps.totalTweetsSeen),
   });
 
   setHeaders({
