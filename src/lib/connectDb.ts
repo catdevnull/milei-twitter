52f6986f98772a9bcdb71b5f179e2770b0431208
Nulo <git@nulo.in>
2024-02-24 15:38:13 -0300

    no migrar
    no se puede migrar en workers

--- src/lib/connectDb.ts
+++ src/lib/connectDb.ts
@@ -1,7 +1,6 @@
 import * as schema from "../schema";
 import { drizzle } from "drizzle-orm/libsql";
 import { createClient } from "@libsql/client";
-import { migrate } from "drizzle-orm/libsql/migrator";
 
 export async function connectDb(DB_PATH?: string) {
   const client = createClient({
@@ -9,6 +8,6 @@ export async function connectDb(DB_PATH?: string) {
     authToken: process.env.TURSO_AUTH_TOKEN!,
   });
   const db = drizzle(client, { schema });
-  await migrate(db, { migrationsFolder: "drizzle" });
+  // await migrate(db, { migrationsFolder: "drizzle" });
   return db;
 }
