8de4949fc958d0790b99725939d58ee86a60a5b5
Nulo <git@nulo.in>
2024-02-28 22:28:45 -0300

    opinion publica prototipo 1

---
+++ sitio/src/routes/experimentos/opinion-publica/placa-1/+page.server.ts
@@ -0,0 +1,27 @@
+import { db } from "$lib/db";
+import { desc, isNotNull } from "drizzle-orm";
+import { scraps } from "../../../../schema";
+import type { PageServerLoad } from "./$types";
+import { queryLastWeek } from "$lib/data-processing/queryWeekly";
+
+export const load: PageServerLoad = async ({ setHeaders }) => {
+  const t0 = performance.now();
+  const [lastUpdated, ultimaSemana] = await Promise.all([
+    db.query.scraps.findFirst({
+      orderBy: desc(scraps.at),
+      where: isNotNull(scraps.totalTweetsSeen),
+    }),
+    queryLastWeek(),
+  ]);
+  const t1 = performance.now();
+  console.log("lastUpdated+ultimaSemana", t1 - t0);
+
+  setHeaders({
+    "cache-control": "public, max-age=60",
+  });
+
+  return {
+    lastUpdated,
+    ultimaSemana,
+  };
+};
