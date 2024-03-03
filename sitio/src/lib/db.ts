ba0e8c1e1999be80e1bc34b00a3031303012a97f
Nulo <git@nulo.in>
2024-03-03 14:25:29 -0300

    litestream, etc

--- sitio/src/lib/db.ts
+++ sitio/src/lib/db.ts
@@ -1,9 +1,8 @@
 import { env } from "$env/dynamic/private";
 import { connectDb } from "./connectDb.js";
 
-const url = env.TURSO_CONNECTION_URL ?? "file:sqlite.db";
+const url = env.DB_PATH ? `file:${env.DB_PATH}` : "file:sqlite.db";
 console.info(`Using db at ${url}`);
 export const db = await connectDb({
   url,
-  authToken: env.TURSO_AUTH_TOKEN,
 });
