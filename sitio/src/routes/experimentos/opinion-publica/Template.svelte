1f7ad206d1b5c96f8dee7b3ec50a06c99004f02b
Nulo <git@nulo.in>
2024-03-27 17:03:34 -0300

    formatear todo

--- sitio/src/routes/experimentos/opinion-publica/Template.svelte
+++ sitio/src/routes/experimentos/opinion-publica/Template.svelte
@@ -12,18 +12,18 @@
 <!-- azul 5ea1b4 / rojo d83926 -->
 
 <div
-  class="font-gilroy flex min-h-screen w-screen flex-col items-center bg-[#5ea1b4] font-bold uppercase text-black"
+  class="flex min-h-screen w-screen flex-col items-center bg-[#5ea1b4] font-gilroy font-bold uppercase text-black"
   style={`background: url(${Fondo})`}
 >
   <div class="mx-auto inline-flex min-w-0 flex-col items-center">
     <h1
-      class="drop-shadow-opinionPublicaAzul min-w-0 px-8 pb-12 pt-16 text-center text-7xl font-extrabold"
+      class="min-w-0 px-8 pb-12 pt-16 text-center text-7xl font-extrabold drop-shadow-opinionPublicaAzul"
     >
       <slot name="title" />
     </h1>
 
     <div
-      class="border-collapse-[separate] drop-shadow-opinionPublica mx-auto box-border inline-flex rounded-3xl border-4 border-black bg-[#5ea1b4] text-4xl"
+      class="border-collapse-[separate] mx-auto box-border inline-flex rounded-3xl border-4 border-black bg-[#5ea1b4] text-4xl drop-shadow-opinionPublica"
     >
       <slot name="content" />
     </div>
