59df705d6cc4f1372ca9791195db32313619413a
Nulo <git@nulo.in>
2024-03-31 20:08:20 -0300

    seedear DB en hooks/agregar parametros de perf a db

--- sitio/src/lib/db/connectDb.ts
+++ sitio/src/lib/db/connectDb.ts
@@ -1,8 +1,6 @@
 import * as schema from "../../schema";
 import { drizzle } from "drizzle-orm/libsql";
 import { createClient } from "@libsql/client";
-import { seedHistoricLikes } from "./seedHistoricLikes";
-
 export async function connectDb({
   url,
   authToken,
@@ -11,7 +9,12 @@ export async function connectDb({
   authToken?: string;
 }) {
   const client = createClient({ url, authToken });
+  await client.execute(`PRAGMA journal_mode = WAL;`);
+  await client.execute(`PRAGMA busy_timeout = 5000;`);
+  await client.execute(`PRAGMA synchronous = NORMAL;`);
+  await client.execute(`PRAGMA cache_size = 1000000000;`);
+  await client.execute(`PRAGMA foreign_keys = true;`);
+  await client.execute(`PRAGMA temp_store = memory;`);
   const db = drizzle(client, { schema });
-  await seedHistoricLikes(db);
   return db;
 }
