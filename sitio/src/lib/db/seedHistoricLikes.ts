0f3f152bff54b0002bb1b61dfc032a29a3675c6d
Nulo <git@nulo.in>
2024-04-01 19:10:06 -0300

    no seedear en una transaccion
    inexplicablemente falla en prod

--- sitio/src/lib/db/seedHistoricLikes.ts
+++ sitio/src/lib/db/seedHistoricLikes.ts
@@ -4,7 +4,6 @@ import { promisify } from "node:util";
 import { brotliDecompress } from "node:zlib";
 import type { connectDb } from "./connectDb";
 import * as schema from "../../schema";
-import { parse } from "date-fns";
 
 const brotliDecompressP = promisify(brotliDecompress);
 
@@ -14,30 +13,27 @@ export async function seedHistoricLikes(
   console.time("seedHistoricLikes");
   const compressed = await readFile("src/lib/db/historicLikes/likes.tsv.br");
   const dataset = await brotliDecompressP(compressed);
-  await db.transaction(async (tx) => {
-    for (const line of dataset.toString().split("\r\n").slice(1)) {
-      const [url, tweet_timestamp, , estimated_like_timestamp] =
-        line.split("\t");
-      const parseado = parsearLinkDeTwitter(url);
-      if (!parseado || !("id" in parseado))
-        throw `no se pudo parsear linea '${line}'`;
-      const values: typeof schema.historicLikedTweets.$inferInsert = {
-        postId: parseado.id,
-        url,
-        postedAt: new Date(tweet_timestamp),
-        estimatedLikedAt: dayjs(
-          estimated_like_timestamp,
-          // 2023-11-29 12:59:46
-          "YYYY-MM-DD HH:mm:ss",
-        )
-          .tz(tz, true)
-          .toDate(),
-      };
-      await tx
-        .insert(schema.historicLikedTweets)
-        .values(values)
-        .onConflictDoNothing();
-    }
-  });
+  for (const line of dataset.toString().split("\r\n").slice(1)) {
+    const [url, tweet_timestamp, , estimated_like_timestamp] = line.split("\t");
+    const parseado = parsearLinkDeTwitter(url);
+    if (!parseado || !("id" in parseado))
+      throw `no se pudo parsear linea '${line}'`;
+    const values: typeof schema.historicLikedTweets.$inferInsert = {
+      postId: parseado.id,
+      url,
+      postedAt: new Date(tweet_timestamp),
+      estimatedLikedAt: dayjs(
+        estimated_like_timestamp,
+        // 2023-11-29 12:59:46
+        "YYYY-MM-DD HH:mm:ss",
+      )
+        .tz(tz, true)
+        .toDate(),
+    };
+    await db
+      .insert(schema.historicLikedTweets)
+      .values(values)
+      .onConflictDoNothing();
+  }
   console.timeEnd("seedHistoricLikes");
 }
