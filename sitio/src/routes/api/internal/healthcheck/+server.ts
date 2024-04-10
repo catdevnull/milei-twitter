b3f23c754e074422d09dc10a920cf2328e273e6f
Nulo <git@nulo.in>
2024-04-10 17:23:20 -0300

    feat: agregar healthcheck
    fixes #53

---
+++ sitio/src/routes/api/internal/healthcheck/+server.ts
@@ -0,0 +1,31 @@
+import { db } from "$lib/db";
+import { desc, isNotNull } from "drizzle-orm";
+import { likedTweets, retweets, scraps } from "../../../../schema";
+import { lastWeek } from "$lib/data-processing/weekly";
+import {
+  calculateScreenTime,
+  totalFromDurations,
+} from "$lib/data-processing/screenTime";
+import dayjs from "dayjs";
+
+export async function GET() {
+  let errors: Array<string> = [];
+  const lastScrap = await db.query.scraps.findFirst({
+    orderBy: desc(scraps.at),
+    where: isNotNull(scraps.totalTweetsSeen),
+  });
+  if (lastScrap) {
+    const delta = +new Date() - +lastScrap.at;
+    if (delta > 10 * 60 * 1000) {
+      errors.push(`último scrap hace ${delta}ms (>10min)`);
+    }
+  } else errors.push("no hay scraps");
+
+  if (errors.length) {
+    return new Response(`errors:\n${errors.map((e) => `- ${e}`).join("\n")}`, {
+      status: 500,
+    });
+  } else {
+    return new Response(`ok (last scrap at ${lastScrap?.at})`);
+  }
+}
