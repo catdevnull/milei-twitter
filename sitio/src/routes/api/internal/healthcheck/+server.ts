a5e1d2a85ea13f989dc7154d32ddae5d3ae9f1b2
Nulo <git@nulo.in>
2024-05-12 15:02:17 -0300

    errorear cuando hace mucho que no vemos likes

--- sitio/src/routes/api/internal/healthcheck/+server.ts
+++ sitio/src/routes/api/internal/healthcheck/+server.ts
@@ -1,12 +1,6 @@
 import { db } from "$lib/db";
-import { and, desc, gt, isNotNull } from "drizzle-orm";
-import { likedTweets, retweets, scraps } from "../../../../schema";
-import { lastWeek } from "$lib/data-processing/weekly";
-import {
-  calculateScreenTime,
-  totalFromDurations,
-} from "$lib/data-processing/screenTime";
-import dayjs from "dayjs";
+import { and, desc, gt, isNotNull, sql } from "drizzle-orm";
+import { likedTweets, scraps } from "../../../../schema";
 
 export async function GET() {
   let errors: Array<string> = [];
@@ -17,6 +11,17 @@ export async function GET() {
       gt(scraps.totalTweetsSeen, 0),
     ),
   });
+  const lastScrapWithLikes = await db
+    .select({
+      finishedAt: scraps.finishedAt,
+      count: sql`(select count(*) from ${likedTweets} where ${scraps.id} = ${likedTweets.scrapId}) as c`,
+    })
+    .from(scraps)
+    .orderBy(desc(scraps.finishedAt))
+    .groupBy()
+    .limit(1)
+    .where(sql`c > 0`);
+
   if (lastScrap) {
     const delta = +new Date() - +lastScrap.finishedAt;
     if (delta > 10 * 60 * 1000) {
@@ -25,9 +30,17 @@ export async function GET() {
     if (lastScrap.totalTweetsSeen && lastScrap.totalTweetsSeen < 10) {
       errors.push(`solo ${lastScrap.totalTweetsSeen} tweets vistos (<10)`);
     }
+  } else errors.push("no hay scraps");
+  if (lastScrapWithLikes && lastScrapWithLikes.length > 0) {
+    const delta = +new Date() - +lastScrapWithLikes[0].finishedAt;
+    if (delta > 1.5 * 60 * 60 * 1000) {
+      errors.push(
+        `último scrap con ${lastScrapWithLikes[0].count} likes hace ${delta}ms (>1:30h)`,
+      );
+    }
     // TODO: especificamente ver el output que nos importa que serían los likes
     // no tengo claro como se lograría eso, quizás agregar un lastSeenAt en los likes y ver que siempre haya likes con un lastSeenAt reciente?
-  } else errors.push("no hay scraps");
+  } else errors.push("no hay scraps con likes");
 
   if (errors.length) {
     return new Response(`errors:\n${errors.map((e) => `- ${e}`).join("\n")}`, {
