758d08c0e5679ab89f59598246c76ae6a885436a
Nulo <git@nulo.in>
2024-05-17 08:45:40 -0300

    (casi) no replayear sesiones sin errores

--- sitio/src/hooks.client.ts
+++ sitio/src/hooks.client.ts
@@ -8,7 +8,7 @@ if (import.meta.env.PROD)
 
     // This sets the sample rate to be 10%. You may want this to be 100% while
     // in development and sample at a lower rate in production
-    replaysSessionSampleRate: 0.5,
+    replaysSessionSampleRate: 0.01,
 
     // If the entire session is not sampled, use the below sample rate to sample
     // sessions when an error occurs.
