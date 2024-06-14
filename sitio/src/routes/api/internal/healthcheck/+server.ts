7251b1313989eeb34265a90c8a720d170d0bf51e
Nulo <git@nulo.in>
2024-06-14 16:51:16 -0300

    update healthcheck
    prepararse para el apocalipsis #59

--- sitio/src/routes/api/internal/healthcheck/+server.ts
+++ sitio/src/routes/api/internal/healthcheck/+server.ts
@@ -1,6 +1,7 @@
 import { db } from "$lib/db";
 import { and, desc, gt, isNotNull, sql } from "drizzle-orm";
-import { likedTweets, scraps } from "../../../../schema";
+import { likedTweets, retweets, scraps } from "../../../../schema";
+import { likesCutoffSql } from "$lib/consts";
 
 export async function GET() {
   let errors: Array<string> = [];
@@ -11,19 +12,30 @@ export async function GET() {
       gt(scraps.totalTweetsSeen, 0),
     ),
   });
-  const lastScrapWithLikes = await db
+  // const lastScrapWithLikes = await db
+  //   .select({
+  //     finishedAt: scraps.finishedAt,
+  //     count: sql`(select count(*) from ${likedTweets} where ${scraps.id} = ${likedTweets.scrapId} and ${likesCutoffSql}) as c`,
+  //   })
+  //   .from(scraps)
+  //   .orderBy(desc(scraps.finishedAt))
+  //   .groupBy()
+  //   .limit(1)
+  //   .where(sql`c > 0`);
+  const lastScrapWithRetweets = await db
     .select({
       finishedAt: scraps.finishedAt,
-      count: sql`(select count(*) from ${likedTweets} where ${scraps.id} = ${likedTweets.scrapId}) as c`,
+      count: sql`(select count(*) from ${retweets} where ${scraps.id} = ${retweets.scrapId}) as c`,
     })
     .from(scraps)
     .orderBy(desc(scraps.finishedAt))
     .groupBy()
     .limit(1)
     .where(sql`c > 0`);
-  const lastLikedTweet = await db.query.likedTweets.findFirst({
-    orderBy: desc(likedTweets.lastSeenAt),
-  });
+  // const lastLikedTweet = await db.query.likedTweets.findFirst({
+  //   orderBy: desc(likedTweets.lastSeenAt),
+  //   where: likesCutoffSql,
+  // });
 
   if (lastScrap) {
     const delta = +new Date() - +lastScrap.finishedAt;
@@ -34,20 +46,20 @@ export async function GET() {
       errors.push(`solo ${lastScrap.totalTweetsSeen} tweets vistos (<10)`);
     }
   } else errors.push("no hay scraps");
-  if (lastScrapWithLikes && lastScrapWithLikes.length > 0) {
-    const delta = +new Date() - +lastScrapWithLikes[0].finishedAt;
+  if (lastScrapWithRetweets && lastScrapWithRetweets.length > 0) {
+    const delta = +new Date() - +lastScrapWithRetweets[0].finishedAt;
     if (delta > 12 * 60 * 60 * 1000) {
       errors.push(
-        `último scrap con ${lastScrapWithLikes[0].count} likes hace ${delta}ms (>12h)`,
+        `último scrap con ${lastScrapWithRetweets[0].count} retweets hace ${delta}ms (>12h)`,
       );
     }
   } else errors.push("no hay scraps con likes");
-  if (lastLikedTweet) {
-    const delta = +new Date() - +(lastLikedTweet.lastSeenAt ?? new Date());
-    if (delta > 10 * 60 * 1000) {
-      errors.push(`último tweet visto hace ${delta}ms (>10min)`);
-    }
-  } else errors.push("no hay ultimo like tweet");
+  // if (lastLikedTweet) {
+  //   const delta = +new Date() - +(lastLikedTweet.lastSeenAt ?? new Date());
+  //   if (delta > 10 * 60 * 1000) {
+  //     errors.push(`último tweet visto hace ${delta}ms (>10min)`);
+  //   }
+  // } else errors.push("no hay ultimo like tweet");
 
   if (errors.length) {
     return new Response(`errors:\n${errors.map((e) => `- ${e}`).join("\n")}`, {
