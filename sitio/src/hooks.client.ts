6e0dec97c011a22de5670decb9b10f7e2c69577b
Nulo <git@nulo.in>
2024-07-09 19:17:38 -0300

    try to properly disable Sentry in dev

--- sitio/src/hooks.client.ts
+++ sitio/src/hooks.client.ts
@@ -1,22 +1,22 @@
 import { handleErrorWithSentry, replayIntegration } from "@sentry/sveltekit";
 import * as Sentry from "@sentry/sveltekit";
 
-if (import.meta.env.PROD)
-  Sentry.init({
-    dsn: "https://79b56150c5092cdad5c56c62223a1a5d@o4507188153548800.ingest.de.sentry.io/4507188155646032",
-    tracesSampleRate: 1.0,
+Sentry.init({
+  dsn: "https://79b56150c5092cdad5c56c62223a1a5d@o4507188153548800.ingest.de.sentry.io/4507188155646032",
+  tracesSampleRate: 1.0,
 
-    // This sets the sample rate to be 10%. You may want this to be 100% while
-    // in development and sample at a lower rate in production
-    replaysSessionSampleRate: 0.01,
+  // This sets the sample rate to be 10%. You may want this to be 100% while
+  // in development and sample at a lower rate in production
+  replaysSessionSampleRate: 0.01,
 
-    // If the entire session is not sampled, use the below sample rate to sample
-    // sessions when an error occurs.
-    replaysOnErrorSampleRate: 1.0,
+  // If the entire session is not sampled, use the below sample rate to sample
+  // sessions when an error occurs.
+  replaysOnErrorSampleRate: 1.0,
 
-    // If you don't want to use Session Replay, just remove the line below:
-    integrations: [replayIntegration()],
-  });
+  // If you don't want to use Session Replay, just remove the line below:
+  integrations: [replayIntegration()],
+  enabled: import.meta.env.PROD,
+});
 
 // If you have a custom error handler, pass it to `handleErrorWithSentry`
 export const handleError = handleErrorWithSentry();
