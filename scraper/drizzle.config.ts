bd4fe3a52a2121f255ced6061a0c747163356853
Nulo <git@nulo.in>
2024-07-13 17:03:07 -0300

    borrar scraper/

--- scraper/drizzle.config.ts
+++
@@ -1,9 +0,0 @@
-import type { Config } from "drizzle-kit";
-export default {
-  schema: "./schema.ts",
-  out: "./drizzle",
-  driver: "better-sqlite",
-  dbCredentials: {
-    url: "sqlite.db",
-  },
-} satisfies Config;
