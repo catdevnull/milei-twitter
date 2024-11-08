bbfb257ddbc25e4f3371131eac5bef0e9c85422f
Nulo <git@nulo.in>
2024-11-07 23:44:38 -0300

    batch fetch

--- sitio/src/routes/api/datasets/retweets.csv/+server.ts
+++ sitio/src/routes/api/datasets/retweets.csv/+server.ts
@@ -1,16 +1,35 @@
 import { db } from "$lib/db";
-import { desc } from "drizzle-orm";
+import { desc, lt } from "drizzle-orm";
 import { retweets } from "../../../../schema";
 import { stringify } from "csv-stringify/sync";
 
 export async function GET() {
   console.time("retweets-csv");
 
-  const retweetsData = await db.query.retweets.findMany({
-    orderBy: desc(retweets.retweetAt),
-  });
+  const BATCH_SIZE = 1000;
+  let lastRetweetAt: Date | null = null;
+  let allRecords: (typeof retweets.$inferSelect)[] = [];
 
-  const records = retweetsData.map((row) => [
+  // workaround because libsql sucks and returns "LibsqlError: RESPONSE_TOO_LARGE: Response is too large"
+  while (true) {
+    const batch: (typeof retweets.$inferSelect)[] =
+      await db.query.retweets.findMany({
+        orderBy: desc(retweets.retweetAt),
+        limit: BATCH_SIZE,
+        where: lastRetweetAt
+          ? lt(retweets.retweetAt, lastRetweetAt)
+          : undefined,
+      });
+
+    if (batch.length === 0) break;
+
+    allRecords = allRecords.concat(batch);
+    lastRetweetAt = batch[batch.length - 1].retweetAt;
+
+    if (batch.length < BATCH_SIZE) break;
+  }
+
+  const records = allRecords.map((row) => [
     row.retweetAt.toISOString(),
     row.postedAt.toISOString(),
     row.posterId,
