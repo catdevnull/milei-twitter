8149f837a87f549a6ddfb93c55974dfbbfd3fd30
Nulo <git@nulo.in>
2024-11-22 21:13:05 -0300

    no usar iframe

--- sitio/src/routes/+page.svelte
+++ sitio/src/routes/+page.svelte
@@ -29,6 +29,7 @@
   import Meta from "$lib/components/Meta.svelte";
   import AlertInfo from "$lib/components/AlertInfo.svelte";
   import { dateToMonthString } from "./promedios/[year]/[month]/months";
+  import { onMount } from "svelte";
 
   export let data: PageData;
 
@@ -155,6 +156,10 @@
       duende = true;
     }
   }
+
+  onMount(() => {
+    window.dispatchEvent(new Event("mounted"));
+  });
 </script>
 
 <Meta keywords={true} canonical={"https://milei.nulo.lol"} />
@@ -401,17 +406,59 @@
     <h2 class="mb-4 text-center text-2xl font-bold md:text-4xl">
       💞 Los favoritos de Milei 💞
     </h2>
-    <iframe
+
+    <div id="observablehq-title-bfd5728c"></div>
+    <div id="observablehq-viewof-top-bfd5728c"></div>
+    <div id="observablehq-chart_1-bfd5728c"></div>
+
+    <!-- <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@observablehq/inspector@5/dist/inspector.css"> -->
+    <!-- prettier-ignore -->
+    <style>
+      /* es el coso de arriba inlineado lol */
+      :root{--syntax_normal:#1b1e23;--syntax_comment:#a9b0bc;--syntax_number:#20a5ba;--syntax_keyword:#c30771;--syntax_atom:#10a778;--syntax_string:#008ec4;--syntax_error:#ffbedc;--syntax_unknown_variable:#838383;--syntax_known_variable:#005f87;--syntax_matchbracket:#20bbfc;--syntax_key:#6636b4;--mono_fonts:82%/1.5 Menlo,Consolas,monospace}.observablehq--collapsed,.observablehq--expanded,.observablehq--function,.observablehq--gray,.observablehq--import,.observablehq--string:after,.observablehq--string:before{color:var(--syntax_normal)}.observablehq--collapsed,.observablehq--inspect a{cursor:pointer}.observablehq--field{text-indent:-1em;margin-left:1em}.observablehq--empty{color:var(--syntax_comment)}.observablehq--blue,.observablehq--keyword{color:#3182bd}.observablehq--forbidden,.observablehq--pink{color:#e377c2}.observablehq--orange{color:#e6550d}.observablehq--boolean,.observablehq--null,.observablehq--undefined{color:var(--syntax_atom)}.observablehq--bigint,.observablehq--date,.observablehq--green,.observablehq--number,.observablehq--regexp,.observablehq--symbol{color:var(--syntax_number)}.observablehq--index,.observablehq--key{color:var(--syntax_key)}.observablehq--prototype-key{color:#aaa}.observablehq--empty{font-style:oblique}.observablehq--purple,.observablehq--string{color:var(--syntax_string)}.observablehq--error,.observablehq--red{color:#e7040f}.observablehq--inspect{font:var(--mono_fonts);overflow-x:auto;display:block;white-space:pre}.observablehq--error .observablehq--inspect{word-break:break-all;white-space:pre-wrap}
+    </style>
+    <script type="module" async>
+      import {
+        Runtime,
+        Inspector,
+      } from "https://cdn.jsdelivr.net/npm/@observablehq/runtime@5/dist/runtime.js";
+      import define from "https://api.observablehq.com/@rusosnith/retuits-milei@261.js?v=4";
+
+      window.addEventListener("mounted", () => {
+        new Runtime().module(define, (name) => {
+          if (name === "title")
+            return new Inspector(
+              document.querySelector("#observablehq-title-bfd5728c"),
+            );
+          if (name === "viewof top")
+            return new Inspector(
+              document.querySelector("#observablehq-viewof-top-bfd5728c"),
+            );
+          if (name === "chart_1")
+            return new Inspector(
+              document.querySelector("#observablehq-chart_1-bfd5728c"),
+            );
+          return ["tuiteroMensual"].includes(name);
+        });
+      });
+    </script>
+    <!-- <iframe
       class="aspect-[800/876] dark:invert"
       width="100%"
       frameborder="0"
       src="https://observablehq.com/embed/@rusosnith/retuits-milei?cells=chart_1"
-    ></iframe>
+    ></iframe> -->
     <p class="my-4 text-center text-sm">
       gracias a <a
-        href="https://observablehq.com/@rusosnith"
-        class="text-blue-600 underline dark:text-blue-200">el ruso</a
-      > por la visualización 🫶
+        href="https://x.com/rusosnith"
+        class="text-blue-600 underline dark:text-blue-300">el ruso</a
+      >
+      por la visualización 🫶 ⋅
+      <a
+        href="https://observablehq.com/@rusosnith/retuits-milei"
+        target="_blank"
+        class="text-blue-600 underline dark:text-blue-300">fuente</a
+      >
     </p>
   </section>
   <div class="py-8">
