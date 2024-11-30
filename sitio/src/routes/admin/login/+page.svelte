e5390af0b04983660644bb35b84ff36d3a74753d
Nulo <git@nulo.in>
2024-11-30 13:31:11 -0300

    fix format

--- sitio/src/routes/admin/login/+page.svelte
+++ sitio/src/routes/admin/login/+page.svelte
@@ -17,7 +17,7 @@
       type="password"
       name="password"
       placeholder="Password"
-      class="ring-offset-background inline-flex h-10 flex-grow rounded-md border border-neutral-300 bg-white px-3 py-2 text-sm placeholder:text-neutral-500 focus:border-neutral-300 focus:outline-none focus:ring-2 focus:ring-neutral-400 focus:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 dark:bg-neutral-900"
+      class="inline-flex h-10 flex-grow rounded-md border border-neutral-300 bg-white px-3 py-2 text-sm ring-offset-background placeholder:text-neutral-500 focus:border-neutral-300 focus:outline-none focus:ring-2 focus:ring-neutral-400 focus:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 dark:bg-neutral-900"
     />
     <button
       class="focus:shadow-outline inline-flex items-center justify-center rounded-md bg-neutral-950 px-4 py-2 text-sm font-medium tracking-wide text-white transition-colors duration-200 hover:bg-neutral-900 focus:outline-none focus:ring-2 focus:ring-neutral-900 focus:ring-offset-2"
