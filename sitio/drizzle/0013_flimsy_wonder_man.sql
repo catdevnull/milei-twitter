9b5e98c5628af4488c16f4aa8a47d90c01f6a8cf
Nulo <git@nulo.in>
2024-12-02 13:45:03 -0300

    derp

---
+++ sitio/drizzle/0013_flimsy_wonder_man.sql
@@ -0,0 +1,2 @@
+ALTER TABLE `db_tweets` ADD `json_timestamp` integer;--> statement-breakpoint
+ALTER TABLE `db_tweets` ADD `json_is_retweet` integer;
---
+++ sitio/drizzle/0014_big_zzzax.sql
@@ -0,0 +1,2 @@
+ALTER TABLE `db_tweets` DROP COLUMN `json_timestamp`;--> statement-breakpoint
+ALTER TABLE `db_tweets` DROP COLUMN `json_is_retweet`;
